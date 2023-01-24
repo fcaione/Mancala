ad2ee93cf61d1cc6b50db8cb4b35646611972d98
Freddy Caione <fac299@nyu.edu>
2023-01-24 10:42:23 -0500

    created board

---
+++ main.js
@@ -0,0 +1,11 @@
+//DOM grab array of divs
+const arrayOfCells = document.querySelectorAll(".cell")
+console.log(arrayOfCells)
+
+//handle click function
+function handleClick() {
+
+}
+
+const gameBoard = document.getElementById("board")
+console.log(gameBoard)
